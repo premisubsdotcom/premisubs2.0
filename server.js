c8760a461afb3b252d7ee31a2785cf0d68485d37
premisubsdotcom <premiumsubscriptionshub@gmail.com>
2025-04-26 17:10:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const createJoinButtons = () => {
   return [
     { text: 'Join Channel 1', url: 'https://t.me/+XY-47U_k6Ks0MWY1' },
     { text: 'Join Channel 2', url: 'https://t.me/+fM4A8zBxpDgxNmQ1' },
-    { text: 'Join Channel 3', url: 'https://t.me/+srv4Hheq0Y00NTQ1' },
+    { text: 'Join Channel 3', url: 'https://t.me/+tgjAXv5NtOxhMjc1' },
     { text: 'Join Channel 4', url: 'https://t.me/+LzG6N6PaAkMwODVl' },
     { text: '→ Join in all channels at once', url: 'https://t.me/addlist/_nkPpuc2DRBmNjFl' }
   ];
@@ -99,7 +99,7 @@ const checkMembership = (chatId, callback) => {
 const channels = [
   -1002512938856, // Channel 1
   -1002661090015, // Channel 2
-  -1002266460123, // Channel 3
+  -1002551200861, // Channel 3
   -1002403206209  // Channel 4
 ];
 
